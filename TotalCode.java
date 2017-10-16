86c5c67e449e3959d9770b1d6059d47ac3cd4929
Nicholas Page <ntpy5@schls.albco>
2017-10-16 16:13:47 -0400

    Revert "Merge branch 'master' of https://github.com/ThorGuy/nerds9064"
    This reverts commit 9b40db11c2da6e136a3d62787ccf304078244c32, reversing
    changes made to f384d4f33b809936c8c9208722f9e9a0dbd2254a.

--- TotalCode.java
+++ TotalCode.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.robotcontroller.external.GitHub.nerds9064;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import org.firstinspires.ftc.robotcontroller.external.GitHub.nerds9064.AutOp;
-import org.firstinspires.ftc.robotcontroller.external.GitHub.nerds9064.Teleop;
 
 /**
  * Created by wbmf2 on 10/9/2017.
