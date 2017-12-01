9c051fde56901a7fa63ce3a9ae3bc4716913e1ca
ThorGuy <32145412+ThorGuy@users.noreply.github.com>
2017-12-01 12:56:18 -0500

    Added movement methods and extended the right opmode

--- AutoOp.java
+++ AutoOp.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.robotcontroller.external.GitHub.nerds9064;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,21 +10,19 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by ntpy5 on 11/27/2017.
  */
 
-public class AutoOp extends OpMode{
-    DcMotor leftFront, leftBack, rightFront, rightBack, arm;
-    Servo servo1, servo2;
-    ColorSensor gemSensor;
-    boolean test = false;
-    public void init(){
+public class AutoOp extends LinearOpMode{
+    private DcMotor leftFront, leftBack, rightFront, rightBack, arm;
+    private Servo servo1, servo2;
+    private ColorSensor gemSensor;
+    private boolean test = false;
+    public void runOpMode() throws InterruptedException{
         Initialize();
+        test();
+        noInputAuto();
     }
-    public void loop(){
-
-    }
-    public void noInputAuto() throws InterruptedException {
-
+    private void noInputAuto()throws InterruptedException {
     /*
-    TODO:
+    TODO: List of Actions
     [ ] Drop arm
     [ ] Sense gem color
     [ ] Sense picture
@@ -33,24 +32,97 @@ public class AutoOp extends OpMode{
     [ ] line up glyph
     [ ] insert glyph
     [ ] back up but stay in safe zone
-
+    TODO: End list
      */
+    }
+    public void test()throws InterruptedException{
+        forward(0.5,2000);
+        backward(0.5,2000);
+        left(0.5,2000);
+        right(0.5,2000);
+        clockwise(0.5,2000);
+        countclock(0.5,2000);
 
-        setMotorPower("Left Front",leftFront,-0.5,true);
-        setMotorPower("Right Front",rightFront,0.5,true);
-        setMotorPower("Left Back",leftBack,-0.5,true);
-        setMotorPower("Right Back",rightBack,0.5,true);
+    }
+    private void forward(double power, int time) throws InterruptedException{
+        setMotorPower("Left Front",leftFront,-power,true);
+        setMotorPower("Right Front",rightFront,power,true);
+        setMotorPower("Left Back",leftBack,-power,true);
+        setMotorPower("Right Back",rightBack,power,true);
 
-        Thread.sleep(2000);
+        Thread.sleep(time);
 
         setMotorPower("Left Front",leftFront,0,true);
         setMotorPower("Right Front",rightFront,0,true);
         setMotorPower("Left Back",leftBack,0,true);
         setMotorPower("Right Back",rightBack,0,true);
-
     }
-    //Won't send power to motors if test mode is on
-    public void setMotorPower(String motorName, DcMotor motor, double power, boolean tel) {
+    private void backward(double power, int time) throws InterruptedException{
+        setMotorPower("Left Front",leftFront,power,true);
+        setMotorPower("Right Front",rightFront,-power,true);
+        setMotorPower("Left Back",leftBack,power,true);
+        setMotorPower("Right Back",rightBack,-power,true);
+
+        Thread.sleep(time);
+
+        setMotorPower("Left Front",leftFront,0,true);
+        setMotorPower("Right Front",rightFront,0,true);
+        setMotorPower("Left Back",leftBack,0,true);
+        setMotorPower("Right Back",rightBack,0,true);
+    }
+    private void right(double power, int time) throws InterruptedException{
+        setMotorPower("Left Front",leftFront,-power,true);
+        setMotorPower("Right Front",rightFront,-power,true);
+        setMotorPower("Left Back",leftBack,power,true);
+        setMotorPower("Right Back",rightBack,power,true);
+
+        Thread.sleep(time);
+
+        setMotorPower("Left Front",leftFront,0,true);
+        setMotorPower("Right Front",rightFront,0,true);
+        setMotorPower("Left Back",leftBack,0,true);
+        setMotorPower("Right Back",rightBack,0,true);
+    }
+    private void left(double power, int time) throws InterruptedException{
+        setMotorPower("Left Front",leftFront,power,true);
+        setMotorPower("Right Front",rightFront,power,true);
+        setMotorPower("Left Back",leftBack,-power,true);
+        setMotorPower("Right Back",rightBack,-power,true);
+
+        Thread.sleep(time);
+
+        setMotorPower("Left Front",leftFront,0,true);
+        setMotorPower("Right Front",rightFront,0,true);
+        setMotorPower("Left Back",leftBack,0,true);
+        setMotorPower("Right Back",rightBack,0,true);
+    }
+    private void clockwise(double power, int time) throws InterruptedException{
+        setMotorPower("Left Front",leftFront,-power,true);
+        setMotorPower("Right Front",rightFront,-power,true);
+        setMotorPower("Left Back",leftBack,-power,true);
+        setMotorPower("Right Back",rightBack,-power,true);
+
+        Thread.sleep(time);
+
+        setMotorPower("Left Front",leftFront,0,true);
+        setMotorPower("Right Front",rightFront,0,true);
+        setMotorPower("Left Back",leftBack,0,true);
+        setMotorPower("Right Back",rightBack,0,true);
+    }
+    private void countclock(double power, int time) throws InterruptedException{
+        setMotorPower("Left Front",leftFront,power,true);
+        setMotorPower("Right Front",rightFront,power,true);
+        setMotorPower("Left Back",leftBack,power,true);
+        setMotorPower("Right Back",rightBack,power,true);
+
+        Thread.sleep(time);
+
+        setMotorPower("Left Front",leftFront,0,true);
+        setMotorPower("Right Front",rightFront,0,true);
+        setMotorPower("Left Back",leftBack,0,true);
+        setMotorPower("Right Back",rightBack,0,true);
+    }
+    private void setMotorPower(String motorName, DcMotor motor, double power, boolean tel) {
         if (tel)telemetry.addData(motorName + " power: ", "" + power);
         if(test) {
 
@@ -58,8 +130,7 @@ public class AutoOp extends OpMode{
             motor.setPower(power);
         }
     }
-    //Won't send power to servos if test mode is on
-    public void setServoPower(String servoName, Servo servo, double power, boolean tel){
+    private void setServoPower(String servoName, Servo servo, double power, boolean tel){
         if(tel)telemetry.addData(servoName+" position: ", ""+power);
         if(test){
 
@@ -67,7 +138,7 @@ public class AutoOp extends OpMode{
             servo.setPosition(power);
         }
     }
-    public void Initialize(){
+    private void Initialize(){
         if(test) { //Motors and Servos get set to null in testing mode so the phone doesn't try and look for them
             leftFront = null;
             rightFront = null;
