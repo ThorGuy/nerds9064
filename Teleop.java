5ab25d690d857aa8857018484be387cb3e1cd685
ThorGuy <32145412+ThorGuy@users.noreply.github.com>
2018-02-14 16:44:52 -0500

    Tweaked servo range

--- Teleop.java
+++ Teleop.java
@@ -205,8 +205,8 @@ public class Teleop extends OpMode{
         }
         else if(gamepad2.x)
         {
-            hamlet=0.5;
-            ophelia=0.5;
+            hamlet=0.45;
+            ophelia=0.55;
             setServoPosition("servo1",rosencrantz,hamlet,true);
             setServoPosition("servo2",guildenstern,ophelia,true);
 
