e6e445b305e55a35e78dd98663da1292d5c30591
William Merone <wbmf2@schls.albco>
2018-02-14 16:32:42 -0500

    Anyway

--- Teleop.java
+++ Teleop.java
@@ -1,5 +1,5 @@
 
-package org.firstinspires.ftc.teamcode.nerds9064;
+package org.firstinspires.ftc.robotcontroller.external.GitHub.nerds9064;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
