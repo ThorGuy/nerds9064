e7426994d17babb3e1572c536c4913146dc540e1
ThorGuy <32145412+ThorGuy@users.noreply.github.com>
2018-02-14 16:09:43 -0500

    Removed stuff for our autonomous scripting

--- Teleop.java
+++ Teleop.java
@@ -1,5 +1,5 @@
-
 package org.firstinspires.ftc.teamcode.nerds9064;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,7 +7,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.*;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -28,7 +27,6 @@ public class Teleop extends OpMode{
 
     private ElapsedTime runTime = new ElapsedTime();
 
-    private BufferedReader fakeIn;
     private String inLine;
     private ArrayList<String> inArgs = new ArrayList<String>();
 
@@ -53,15 +51,6 @@ public class Teleop extends OpMode{
         Initialize(test); //Initialize servos and motors
 
         //Initialize file reading
-        try{
-            BufferedReader fakeIn = new BufferedReader(new FileReader(autoFileName)); //input.txt can be any existing file
-            inLine = fakeIn.readLine();
-            inArgs.clear();
-            inArgs.addAll(Arrays.asList(inLine.split(",")));
-        }catch(Exception ex){
-            //Uh oh there's an error
-        }
-
         runTime.reset(); //Reset the timer
     }
 
@@ -344,179 +333,4 @@ public class Teleop extends OpMode{
         return out;
     }
     //Overwrites inputs if testing mode is on
-    public void getInputs(){
-        try {
-            if (test) {
-                //Turns everything off once it reaches the end of the file
-                if (false&&fakeIn != null) {
-                    //Test to see if the inputs need to be switched
-
-                    //File reading
-                    if (Double.parseDouble(inArgs.get(0)) <= runTime.milliseconds()) {
-                        do {
-                            inLine = fakeIn.readLine();
-                            inArgs.clear();
-                            inArgs.addAll(Arrays.asList(inLine.split(",")));
-                        } while (inArgs.get(0).startsWith("//"));
-                    }
-
-                    for (int i = 1; i < inArgs.size() - 1; i += 2) {
-                        //Stupidly big switch statement that sets the inputs
-                        switch (inArgs.get(i)) {
-                            case "a1":
-                                gamepad1.a = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "b1":
-                                gamepad1.b = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "x1":
-                                gamepad1.x = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "y1":
-                                gamepad1.y = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "dpad_down1":
-                                gamepad1.dpad_down = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "dpad_left1":
-                                gamepad1.dpad_left = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "dpad_right1":
-                                gamepad1.dpad_right = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "dpad_up1":
-                                gamepad1.dpad_up = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "left_bumper1":
-                                gamepad1.left_bumper = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "left_stick_button1":
-                                gamepad1.left_stick_button = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "left_trigger1":
-                                gamepad1.left_trigger = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "left_stick_x1":
-                                gamepad1.left_stick_x = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "left_stick_y1":
-                                gamepad1.left_stick_y = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "right_bumper1":
-                                gamepad1.right_bumper = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "right_stick_button1":
-                                gamepad1.right_stick_button = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "right_trigger1":
-                                gamepad1.right_trigger = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "right_stick_x1":
-                                gamepad1.right_stick_x = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "right_stick_y1":
-                                gamepad1.right_stick_y = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "a2":
-                                gamepad2.a = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "b2":
-                                gamepad2.b = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "x2":
-                                gamepad2.x = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "y2":
-                                gamepad2.y = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "dpad_down2":
-                                gamepad2.dpad_down = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "dpad_left2":
-                                gamepad2.dpad_left = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "dpad_right2":
-                                gamepad2.dpad_right = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "dpad_up2":
-                                gamepad2.dpad_up = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "left_bunmper2":
-                                gamepad2.left_bumper = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "left_stick_button2":
-                                gamepad2.left_stick_button = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "left_trigger2":
-                                gamepad2.left_trigger = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "left_stick_x2":
-                                gamepad2.left_stick_x = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "left_stick_y2":
-                                gamepad2.left_stick_y = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "right_bumper2":
-                                gamepad2.right_bumper = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "right_stick_button2":
-                                gamepad2.right_stick_button = Boolean.parseBoolean(inArgs.get(i + 1));
-                                break;
-                            case "right_trigger2":
-                                gamepad2.right_trigger = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "right_stick_x2":
-                                gamepad2.right_stick_x = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            case "right_stick_y2":
-                                gamepad2.right_stick_y = Float.parseFloat(inArgs.get(i + 1));
-                                break;
-                            default:
-                                telemetry.addData("ERROR:", "Unsupported input \"" + inArgs.get(i) + "\"");
-                        }
-                    }
-
-                } else {
-                    gamepad1.a = false;
-                    gamepad1.b = false;
-                    gamepad1.x = false;
-                    gamepad1.y = false;
-                    gamepad1.dpad_down = false;
-                    gamepad1.dpad_left = false;
-                    gamepad1.dpad_right = false;
-                    gamepad1.dpad_up = false;
-                    gamepad1.left_bumper = false;
-                    gamepad1.left_stick_button = false;
-                    gamepad1.left_trigger = 0;
-                    gamepad1.left_stick_x = 0;
-                    gamepad1.left_stick_y = 0;
-                    gamepad1.right_bumper = false;
-                    gamepad1.right_stick_button = false;
-                    gamepad1.right_trigger = 0;
-                    gamepad1.right_stick_x = 0;
-                    gamepad1.right_stick_y = 0;
-                    gamepad2.a = false;
-                    gamepad2.b = false;
-                    gamepad2.x = false;
-                    gamepad2.y = false;
-                    gamepad2.dpad_down = false;
-                    gamepad2.dpad_left = false;
-                    gamepad2.dpad_right = false;
-                    gamepad2.dpad_up = false;
-                    gamepad2.left_bumper = false;
-                    gamepad2.left_stick_button = false;
-                    gamepad2.left_trigger = 0;
-                    gamepad2.left_stick_x = 0;
-                    gamepad2.left_stick_y = 0;
-                    gamepad2.right_bumper = false;
-                    gamepad2.right_stick_button = false;
-                    gamepad2.right_trigger = 0;
-                    gamepad2.right_stick_x = 0;
-                    gamepad2.right_stick_y = 0;
-                }
-            }
-        }catch(Exception ex){
-
-        }
-    }
 }
